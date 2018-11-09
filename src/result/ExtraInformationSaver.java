08c15fed76feb482b34bf50aaf2f1b38507b0929
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-09 21:22:53 +0100

    Create class that saves extra info to txt file

---
+++ src/result/ExtraInformationSaver.java
@@ -0,0 +1,27 @@
+package result;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ExtraInformationSaver {
+    private String fileName;
+    private ExtraInformation extraInformation;
+
+    public ExtraInformationSaver(String fileName, ExtraInformation extraInformation) {
+        this.fileName = fileName;
+        this.extraInformation = extraInformation;
+    }
+
+    public void save() {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(fileName))) {
+            printWriter.println(extraInformation.getSolutionLength());
+            printWriter.println(extraInformation.getVisitedStates());
+            printWriter.println(extraInformation.getProcessedStates());
+            printWriter.println(extraInformation.getMaxRecursionDepth());
+            printWriter.println(extraInformation.getComputationTime());
+        } catch (IOException e) {
+            System.out.println("Unable to write extra information to file");
+        }
+    }
+}

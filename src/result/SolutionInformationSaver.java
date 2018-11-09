e892c29e69870026bb69f574820d6008b849abfc
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-09 21:34:29 +0100

    Create class that saves solution info to txt file

---
+++ src/result/SolutionInformationSaver.java
@@ -0,0 +1,29 @@
+package result;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SolutionInformationSaver implements FileSaver {
+    private String fileName;
+    private SolutionInformation solutionInformation;
+
+    public SolutionInformationSaver(String fileName, SolutionInformation solutionInformation) {
+        this.fileName = fileName;
+        this.solutionInformation = solutionInformation;
+    }
+
+    public void save() {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(fileName))) {
+            printWriter.println(solutionInformation.getSolutionLength());
+
+            if (solutionInformation.getSolutionLength() == -1) {
+                return;
+            }
+
+            printWriter.println(solutionInformation.getSolutionMoves());
+        } catch (IOException e) {
+            System.out.println("Unable to write solution information to file");
+        }
+    }
+}

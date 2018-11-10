0a5b016a3caa88b6e807e4beedb6bef51084cbe5
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-10 23:42:29 +0100

    Create ManhattanHeuristic class for future implementation
    It will be used for calculating Manhattan distance for states.

---
+++ src/engine/heuristic/ManhattanHeuristic.java
@@ -0,0 +1,10 @@
+package engine.heuristic;
+
+import engine.State;
+
+public class ManhattanHeuristic implements Heuristic {
+    @Override
+    public int getValue(State processedState, State solvedState) {
+        return 0;
+    }
+}

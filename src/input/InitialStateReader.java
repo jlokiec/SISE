53dcab7574c2de490658ddfefd396667e4e959f0
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-10 23:38:40 +0100

    Use correct State constructor in InitialStateReader

--- src/input/InitialStateReader.java
+++ src/input/InitialStateReader.java
@@ -1,5 +1,6 @@
 package input;
 
+import engine.MoveDirection;
 import engine.State;
 
 import java.io.BufferedReader;
@@ -35,7 +36,7 @@ public class InitialStateReader implements StateReader {
                 }
             }
 
-            initialState = new State(sizeX, sizeY, initialValues);
+            initialState = new State(sizeX, sizeY, initialValues, null, MoveDirection.NO_MOVE, 0);
         } catch (FileNotFoundException e) {
             System.out.println("Unable to read initial state, file not found");
         } catch (IOException e) {

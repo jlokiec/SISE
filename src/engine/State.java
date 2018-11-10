487b0b52946a98b80904c19662f4d57dd1a2685e
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-10 23:30:27 +0100

    Improve State class
    Add parentState, previousMove and depthLevel fields. Create constructor
    that assigns values to all necessary fields. Remove zeroIndex field and
    findZeroIndex method. Index of zero puzzle now can be accessed by
    calling getZeroIndex method. Remove unnecessary constructors and
    setters. Add getPath method that returns solution path.

--- src/engine/State.java
+++ src/engine/State.java
@@ -7,73 +7,67 @@ public class State {
     private byte sizeX;
     private byte sizeY;
     private byte[] stateArray;
-    private byte zeroIndex;
+    private State parentState;
+    private MoveDirection previousMove;
+    private int depthLevel;
 
-    public State() {
-    }
-
-    public State(byte sizeX, byte sizeY) {
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
-    }
-
-    public State(byte sizeX, byte sizeY, byte[] stateArray) {
+    public State(byte sizeX, byte sizeY, byte[] stateArray, State parentState, MoveDirection previousMove, int depthLevel) {
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.stateArray = stateArray;
+        this.parentState = parentState;
+        this.previousMove = previousMove;
+        this.depthLevel = depthLevel;
     }
 
     public byte getSizeX() {
         return sizeX;
     }
 
-    public void setSizeX(byte sizeX) {
-        this.sizeX = sizeX;
-    }
-
     public byte getSizeY() {
         return sizeY;
-    }
-
-    public void setSizeY(byte sizeY) {
-        this.sizeY = sizeY;
     }
 
     public byte[] getStateArray() {
         return stateArray;
     }
 
-    public void setStateArray(byte[] stateArray) {
-        this.stateArray = stateArray;
-    }
-
     public byte getZeroIndex() {
-        findZeroIndex();
-        return zeroIndex;
-    }
-
-    public void findZeroIndex() {
         for (byte i = 0; i < sizeX * sizeY; i++) {
             if (stateArray[i] == 0) {
-                zeroIndex = i;
-                return;
+                return i;
             }
         }
+
+        return -1;
+    }
+
+    public int getDepthLevel() {
+        return depthLevel;
+    }
+
+    public String getPath() {
+        String path = "";
+
+        if (parentState == null) {
+            return path;
+        }
+
+        path = path.concat(parentState.getPath() + previousMove);
+
+        return path;
     }
 
     public byte getZeroX() {
-        return (byte) (zeroIndex % sizeX);
+        return (byte) (getZeroIndex() % sizeX);
     }
 
     public byte getZeroY() {
-        return (byte) ((zeroIndex - getZeroX()) / sizeX);
+        return (byte) ((getZeroIndex() - getZeroX()) / sizeX);
     }
 
     public List<MoveDirection> getAvailableMoves() {
         List<MoveDirection> availableMoves = new ArrayList<>();
-
-        // update zero index value before checking available move directions
-        findZeroIndex();
 
         if (getZeroY() > 0) {
             availableMoves.add(MoveDirection.UP);

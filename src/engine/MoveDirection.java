1a4163381868023ddd6223004b8b8c7e9e59eb76
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-10 23:23:12 +0100

    Improve MoveDirection enum
    Add short string representation of each direction. Add NO_MOVE
    direction.

--- src/engine/MoveDirection.java
+++ src/engine/MoveDirection.java
@@ -1,5 +1,16 @@
 package engine;
 
 public enum MoveDirection {
-    LEFT, RIGHT, UP, DOWN
+    LEFT("L"), RIGHT("R"), UP("U"), DOWN("D"), NO_MOVE("NO");
+
+    private final String directionShort;
+
+    MoveDirection(String directionShort) {
+        this.directionShort = directionShort;
+    }
+
+    @Override
+    public String toString() {
+        return directionShort;
+    }
 }

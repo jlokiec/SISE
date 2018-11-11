7be3c8e0ce76fa1d7a391add13f2476923bfc3f8
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-11 14:33:24 +0100

    Add no-arg constructor and setters in ExtraInformation

--- src/result/ExtraInformation.java
+++ src/result/ExtraInformation.java
@@ -7,6 +7,9 @@ public class ExtraInformation {
     private int maxRecursionDepth;
     private double computationTime;
 
+    public ExtraInformation() {
+    }
+
     public ExtraInformation(int solutionLength, int visitedStates, int processedStates, int maxRecursionDepth, double computationTime) {
         this.solutionLength = solutionLength;
         this.visitedStates = visitedStates;
@@ -19,19 +22,39 @@ public class ExtraInformation {
         return solutionLength;
     }
 
+    public void setSolutionLength(int solutionLength) {
+        this.solutionLength = solutionLength;
+    }
+
     public int getVisitedStates() {
         return visitedStates;
+    }
+
+    public void setVisitedStates(int visitedStates) {
+        this.visitedStates = visitedStates;
     }
 
     public int getProcessedStates() {
         return processedStates;
     }
 
+    public void setProcessedStates(int processedStates) {
+        this.processedStates = processedStates;
+    }
+
     public int getMaxRecursionDepth() {
         return maxRecursionDepth;
     }
 
+    public void setMaxRecursionDepth(int maxRecursionDepth) {
+        this.maxRecursionDepth = maxRecursionDepth;
+    }
+
     public double getComputationTime() {
         return computationTime;
+    }
+
+    public void setComputationTime(double computationTime) {
+        this.computationTime = computationTime;
     }
 }

29e772f9cbe7b51e7433fe6597d8a225f1b3332f
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-11 14:39:56 +0100

    Implement ManhattanHeuristic

--- src/engine/heuristic/ManhattanHeuristic.java
+++ src/engine/heuristic/ManhattanHeuristic.java
@@ -5,6 +5,24 @@ import engine.State;
 public class ManhattanHeuristic implements Heuristic {
     @Override
     public int getValue(State processedState, State solvedState) {
-        return 0;
+        byte[] stateArray = processedState.getStateArray();
+
+        int distance = processedState.getDepthLevel();
+
+        for (int i = 0; i < stateArray.length; i++) {
+            if (stateArray[i] == 0) {
+                continue;
+            }
+
+            byte puzzleValue = stateArray[i];
+            byte puzzleX = processedState.getPuzzleX(puzzleValue);
+            byte puzzleY = processedState.getPuzzleY(puzzleValue);
+            byte solvedX = solvedState.getPuzzleX(puzzleValue);
+            byte solvedY = solvedState.getPuzzleY(puzzleValue);
+
+            distance += Math.abs(puzzleX - solvedX) + Math.abs(puzzleY - solvedY);
+        }
+
+        return distance;
     }
 }

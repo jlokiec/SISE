61dac74ad3a6043656d0606bf9faa39fdef61c9c
Jakub Łokieć <jakublokiec@wp.pl>
2018-11-11 14:32:30 +0100

    Add no-arg constructor and setters in SolutionInformation

--- src/result/SolutionInformation.java
+++ src/result/SolutionInformation.java
@@ -4,6 +4,9 @@ public class SolutionInformation {
     private int solutionLength;
     private String solutionMoves;
 
+    public SolutionInformation() {
+    }
+
     public SolutionInformation(int solutionLength, String solutionMoves) {
         this.solutionLength = solutionLength;
         this.solutionMoves = solutionMoves;
@@ -13,7 +16,15 @@ public class SolutionInformation {
         return solutionLength;
     }
 
+    public void setSolutionLength(int solutionLength) {
+        this.solutionLength = solutionLength;
+    }
+
     public String getSolutionMoves() {
         return solutionMoves;
+    }
+
+    public void setSolutionMoves(String solutionMoves) {
+        this.solutionMoves = solutionMoves;
     }
 }
